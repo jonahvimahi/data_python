08aa7e4e895405fbca5b7d8310f2d95ba688f60d
jonahvimahi <jonahvimahi@gmail.com>
2021-08-09 22:42:32 -0600

    final commit

--- data_presenter.py
+++ data_presenter.py
@@ -1,46 +1,55 @@
+import plotly.graph_objects as go
+
+
 cupcakes = open('CupcakeInvoices.csv')
 
-# for line in cupcakes:
-#     print(line)
+# # for line in cupcakes:
+# #     print(line)
 
-total_chocolate = 0
-total_vanilla = 0
-total_strawberry = 0
+# total_chocolate = 0
+# total_vanilla = 0
+# total_strawberry = 0
 
-for types in cupcakes:
+# for types in cupcakes:
 
-    if "Chocolate" in types:
-        total_chocolate += 1
-    elif "Vanilla" in types:
-        total_vanilla += 1
-    elif "Strawberry" in types:
-        total_strawberry += 1
+#     if "Chocolate" in types:
+#         total_chocolate += 1
+#     elif "Vanilla" in types:
+#         total_vanilla += 1
+#     elif "Strawberry" in types:
+#         total_strawberry += 1
 
-# print(total_chocolate)
-# print(total_vanilla)
-# print(total_strawberry)
+# # print(total_chocolate)
+# # print(total_vanilla)
+# # print(total_strawberry)
 
-for invoice in cupcakes:
+# for invoice in cupcakes:
     
-    value = invoice.split(',')
+#     value = invoice.split(',')
 
-    person_total = int(value[3]) * float(value[4])
+#     person_total = int(value[3]) * float(value[4])
 
-    print(person_total)
+#     print(person_total)
 # next part 
-grand_total = []
 
-for invoice in cupcakes:
+chocolate = 0
+strawberry = 0
+vanilla = 0
 
-  value = invoice.split(',')
+for line in cupcakes:
+    line = line.split(',')
+    if line[2] == 'Chocolate':
+        chocolate += int(line[3]) * float(line[4])
+    elif line[2] == 'Strawberry':
+        strawberry += int(line[3]) * float(line[4])
+    elif line[2] == 'Vanilla':
+        vanilla += int(line[3]) * float(line[4])
 
-  person_total = int(value[3]) * float(value[4])
+grand_total = chocolate + strawberry + vanilla
+print(round(grand_total, 2))
+print(chocolate + strawberry + vanilla)
 
-  grand_total.append(person_total)
+fig = go.Figure(data=go.Bar(y=[chocolate,vanilla,strawberry],x=["Chocolate", "Vanilla", "Strawberry"]))
 
-  # print(person_total)
-  # print(grand_total)
+fig.show()
 
-  print (sum(grand_total))
-
-  cupcakes.close()
